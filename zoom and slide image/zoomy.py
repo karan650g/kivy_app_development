28afdbe9b980c4f858f608edea4fc0ffc3bfbafb
karan gupta <karan.17bit1077@abes.ac.in>
2020-01-07 18:03:20 +0530

    Update zoomy.py

--- zoom and slide image/zoomy.py
+++ zoom and slide image/zoomy.py
@@ -1,1 +1,166 @@
+from kivy.app import App
+from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scatterlayout import ScatterLayout
+from kivy.uix.scatter import Scatter
+from kivy.core.window import Window
+from kivy.graphics.transformation import Matrix
 
+from kivy.lang import Builder
+Builder.load_file('scaler.kv')
+
+class Zoom(ScatterLayout):
+    move_lock = False
+    scale_lock_left = False
+    scale_lock_right = False
+    scale_lock_top = False
+    scale_lock_bottom = False
+    def on_touch_up(self, touch):
+        self.move_lock = False
+        self.scale_lock_left = False
+        self.scale_lock_right = False
+        self.scale_lock_top = False
+        self.scale_lock_bottom = False
+        if touch.grab_current is self:
+            touch.ungrab(self)
+            x = self.pos[0] / 10
+            x = round(x, 0)
+            x = x * 10
+            y = self.pos[1] / 10
+            y = round(y, 0)
+            y = y * 10
+            self.pos = x, y
+            return super(Zoom, self).on_touch_up(touch)
+
+    def transform_with_touch(self, touch):
+        changed = False
+        x = self.bbox[0][0]
+        y = self.bbox[0][1]
+        width = self.bbox[1][0]
+        height = self.bbox[1][1]
+        mid_x = x + width / 2
+        mid_y = y + height / 2
+        inner_width = width * 0.5
+        inner_height = height * 0.5
+        left = mid_x - (inner_width / 2)
+        right = mid_x + (inner_width / 2)
+        top = mid_y + (inner_height / 2)
+        bottom = mid_y - (inner_height / 2)
+
+        if len(self._touches) == self.translation_touches:
+
+            dx = (touch.x - self._last_touch_pos[touch][0]) \
+                 * self.do_translation_x
+            dy = (touch.y - self._last_touch_pos[touch][1]) \
+                 * self.do_translation_y
+            dx = dx / self.translation_touches
+            dy = dy / self.translation_touches
+            if (touch.x > left and touch.x < right and touch.y < top and touch.y > bottom or self.move_lock) and not self.scale_lock_left and not self.scale_lock_right and not self.scale_lock_top and not self.scale_lock_bottom:
+                self.move_lock = True
+                self.apply_transform(Matrix().translate(dx, dy, 0))
+                changed = True
+
+        change_x = touch.x - self.prev_x
+        change_y = touch.y - self.prev_y
+        anchor_sign = 1
+        sign = 1
+        if abs(change_x) >= 9 and not self.move_lock and not self.scale_lock_top and not self.scale_lock_bottom:
+            if change_x < 0:
+                sign = -1
+            if (touch.x < left or self.scale_lock_left) and not self.scale_lock_right:
+                self.scale_lock_left = True
+                self.pos = (self.pos[0] + (sign * 10), self.pos[1])
+                anchor_sign = -1
+            elif (touch.x > right or self.scale_lock_right) and not self.scale_lock_left:
+                self.scale_lock_right = True
+            self.size[0] = self.size[0] + (sign * anchor_sign * 10)
+            self.prev_x = touch.x
+            changed = True
+        if abs(change_y) >= 9 and not self.move_lock and not self.scale_lock_left and not self.scale_lock_right:
+            if change_y < 0:
+                sign = -1
+            if (touch.y > top or self.scale_lock_top) and not self.scale_lock_bottom:
+                self.scale_lock_top = True
+            elif (touch.y < bottom or self.scale_lock_bottom) and not self.scale_lock_top:
+                self.scale_lock_bottom = True
+                self.pos = (self.pos[0], self.pos[1] + (sign * 10))
+                anchor_sign = -1
+            self.size[1] = self.size[1] + (sign * anchor_sign * 10)
+            self.prev_y = touch.y
+            changed = True
+        return changed
+
+    def on_touch_down(self, touch):
+        x, y = touch.x, touch.y
+        self.prev_x = touch.x
+        self.prev_y = touch.y
+
+        if touch.is_mouse_scrolling:
+            if touch.button == 'scrolldown':
+                if self.scale < 100:
+                    self.scale = self.scale * 1.1
+
+            elif touch.button == 'scrollup':
+                if self.scale > 1:
+                    self.scale = self.scale * 0.8
+
+        if not self.do_collide_after_children:
+            if not self.collide_point(x, y):
+                return False
+
+        touch.push()
+        touch.apply_transform_2d(self.to_local)
+        if super(Scatter, self).on_touch_down(touch):
+            if 'multitouch_sim' in touch.profile:
+                touch.multitouch_sim = True
+            touch.pop()
+            self._bring_to_front(touch)
+            return True
+        touch.pop()
+
+        if not self.do_translation_x and \
+                not self.do_translation_y and \
+                not self.do_rotation and \
+                not self.do_scale:
+            return False
+
+        if self.do_collide_after_children:
+            if not self.collide_point(x, y):
+                return False
+
+        if 'multitouch_sim' in touch.profile:
+            touch.multitouch_sim = True
+        self._bring_to_front(touch)
+        touch.grab(self)
+        self._touches.append(touch)
+        self._last_touch_pos[touch] = touch.pos
+
+        return True
+
+class Main_app(BoxLayout,Image):
+    def __init__(self, **kwargs):
+        super(Main_app, self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(None, self)
+        if not self._keyboard:
+            return
+        self._keyboard.bind(on_key_down=self.on_keyboard_down)
+
+    def on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'left':
+            self.x -= 10
+        elif keycode[1] == 'right':
+            self.x += 10
+        elif keycode[1] == 'up':
+            self.y += 10
+        elif keycode[1] == 'down':
+            self.y -= 10
+        return True
+
+class Moveim(App):
+    def build(self):
+        Window.size = (300,600)
+        img = Main_app(source='python_discord_logo.png')
+        return Main_app()
+
+if __name__ ==  '__main__':
+    Moveim().run()
